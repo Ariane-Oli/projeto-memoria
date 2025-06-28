04349f1dfe37df72caba6846a6ae4d9cf839491f
Ariane <ariane.de@outlook.com>
2025-06-28 08:36:17 -0300

    feat: adding new class Atualizador.java

--- src/main/java/org/example/Loja.java
+++ src/main/java/org/example/Loja.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Loja {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Produto p1, p2;
+
+        p1 = new Produto(1, "Computador", 1000);
+
+        System.out.println("Antes da correção.....");
+        System.out.println("pq = " +p1.exibirInfo());
+        Atualizador a = new Atualizador(7.5);
+        a.atualizarPreco(p1);
+
+        System.out.println("Após a correção de preços...");
+        System.out.println("pq = " +p1.exibirInfo());
     }
 }
